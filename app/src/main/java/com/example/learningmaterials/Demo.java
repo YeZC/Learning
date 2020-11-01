14cc7ffb2a446192c4d47e3ea1acc1db0aff3382
叶沼初 <yezhaochu@yezhaochudeMacBook.local>
2020-11-01 23:43:40 +0800

    多线程demo

---
+++ app/src/main/java/com/example/learningmaterials/Demo.java
@@ -0,0 +1,11 @@
+package com.example.learningmaterials;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        ArrayList a = new ArrayList<>();
+    }
+}
