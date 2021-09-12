cdf191ead1ff4c162f954687e89c88062c97309e
yezhaochu <1159473528@qq.com>
2021-09-12 14:25:41 +0800

    add: 手动打包apk

--- gradle-plugins/gradle-plugin-demo/src/main/java/com/yzc/gradle_plugins/MainActivity.java
+++ gradle-plugins/gradle-plugin-demo/src/main/java/com/yzc/gradle_plugins/MainActivity.java
@@ -1,9 +1,9 @@
 package com.yzc.gradle_plugins;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
